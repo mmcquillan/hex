55fd1aa8598c7ee01cab96fb57c18c7e1d25fcfc
Matt McQuillan <matt@mcquillan.org>
2015-11-02 00:44:07 -0500

    Fixing rss debug value and updated sample.config

--- listeners/rss.go
+++ listeners/rss.go
@@ -22,7 +22,7 @@ func Rss(config *configs.Config, listener configs.Listener) {
 		var messages []relays.Message
 		for i := len(feed.Items) - 1; i >= 0; i-- {
 			if lastMarker == "" {
-				lastMarker = feed.Items[2].Date.String()
+				lastMarker = feed.Items[0].Date.String()
 			}
 			item := feed.Items[i]
 			if item.Date.String() > lastMarker {
