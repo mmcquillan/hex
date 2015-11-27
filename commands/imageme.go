aa839a071dd7d8f72e0aea3ef6de8607fa1c487e
Ken Winner <kcswinner@gmail.com>
2015-11-26 20:18:59 -0500

    Cleaned up the code. Added some safety checks. Refactored out the cx

--- commands/imageme.go
+++ commands/imageme.go
@@ -25,47 +25,62 @@ var client = &http.Client {
 }
 
 var baseUrl = "https://www.googleapis.com/customsearch/v1?key="
-var cx = "&cx=001525354505425389698:itoebrubry4"
+var errorMessage = "Error retrieving image"
+var animated bool
 
 func ImageMe(msg string, command models.Command) string {
   msg = strings.TrimSpace(strings.Replace(msg, command.Match, "", 1))
   start := rand.Intn(3)
+  if start < 1 {
+    start = 1
+  }
+
+  if strings.Contains(command.Match, "animate") {
+    animated = true;
+  }
 
   apiKey := command.ApiKey
+  cx := "&cx=" + command.Args
   returnFields := fmt.Sprintf("&fields=items(link)&start=%v", start)
   query := "&q=" + url.QueryEscape(msg)
   fields := "&searchType=image"
+  if animated {
+    fields += "&fileType=gif&hq=animated&tbs=itp:animated"
+  }
+
   url := baseUrl + apiKey + cx + returnFields + query + fields
 
   resp, err := client.Get(url)
   if err != nil {
     log.Print(err)
-    return "Error retrieving image"
+    fmt.Println(err)
+    return errorMessage
   }
   defer resp.Body.Close()
 
   if resp.StatusCode != 200 {
-    FindDeprecatedImage(msg)
+    FindDeprecatedImage(msg, animated)
   }
 
   body, err := ioutil.ReadAll(resp.Body)
   if err != nil {
     log.Print(err)
-    return "Error retrieving image"
+    return errorMessage
   }
 
   var result SearchResult
   err = json.Unmarshal(body, &result)
   if err != nil {
     log.Print(err)
-    return "Error retrieving image"
+    fmt.Println(err)
+    return errorMessage
   }
 
   if len(result.Items) > 0 {
     randomLink := result.Items[rand.Intn(len(result.Items))]
     return randomLink.Link
   } else {
-    return FindDeprecatedImage(msg)
+    return FindDeprecatedImage(msg, animated)
   }
 }
 
@@ -81,28 +96,37 @@ type Result struct {
   Url string `json:"url"`
 }
 
-func FindDeprecatedImage(query string) string {
-  baseUrl := "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&q="
-  url := baseUrl + url.QueryEscape(query)
+func FindDeprecatedImage(query string, animated bool) string {
+  baseUrl := "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8"
+  if animated {
+    baseUrl += "&as_filetype=gif"
+  }
 
-  resp, err := client.Get(url)
+  baseUrl += "&q="
+  searchUrl := baseUrl + url.QueryEscape(query)
+
+  if animated {
+    searchUrl += url.QueryEscape(" animated")
+  }
+
+  resp, err := client.Get(searchUrl)
   if err != nil {
     log.Print(err)
-    return "Error retrieving image"
+    return errorMessage
   }
   defer resp.Body.Close()
 
   body, err := ioutil.ReadAll(resp.Body)
   if err != nil {
     log.Print(err)
-    return "Error retrieving image"
+    return errorMessage
   }
 
   var result DeprecatedResult
   err = json.Unmarshal(body, &result)
   if err != nil {
     log.Print(err)
-    return "Error retrieving image"
+    return errorMessage
   }
 
   index := rand.Intn(len(result.ResponseData.Results))
