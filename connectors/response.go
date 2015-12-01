4ab317c23c1085435b418954f47891085c370bea
Matt McQuillan <matt@mcquillan.org>
2015-11-30 20:46:51 -0500

    added more wildcard matches for responses fixes #66

--- connectors/response.go
+++ connectors/response.go
@@ -20,8 +20,23 @@ func (x Response) Command(message models.Message, publishMsgs chan<- models.Mess
 		log.Printf("%+v", message)
 	}
 	if message.In.Process {
+		txt := strings.ToLower(message.In.Text)
 		for _, c := range connector.Commands {
-			if strings.HasPrefix(strings.ToLower(message.In.Text), strings.ToLower(c.Match)) {
+			m := strings.ToLower(c.Match)
+			ws := strings.HasPrefix(m, "*")
+			we := strings.HasSuffix(m, "*")
+			m = strings.Replace(m, "*", "", -1)
+			eval := false
+			if ws && ws && strings.Contains(txt, m) {
+				eval = true
+			} else if ws && !we && strings.HasPrefix(txt, m) {
+				eval = true
+			} else if !ws && we && strings.HasSuffix(txt, m) {
+				eval = true
+			} else if txt == m {
+				eval = true
+			}
+			if eval {
 				if len(c.Outputs) == 0 {
 					message.Out.Text = strings.Replace(c.Output, "%msg%", strings.TrimSpace(strings.Replace(message.In.Text, c.Match, "", 1)), -1)
 				} else {
@@ -43,7 +58,7 @@ func (x Response) Help(connector models.Connector) (help string) {
 		if c.Help != "" {
 			help += c.Help + "\n"
 		} else {
-			help += c.Match + "\n"
+			help += strings.Replace(c.Match, "*", "", -1) + "\n"
 		}
 	}
 	return help
