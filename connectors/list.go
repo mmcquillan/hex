93223ce14b041bfab1d470867dee23b2083145a3
Matt McQuillan <matt@mcquillan.org>
2015-12-10 14:52:24 -0500

    initial alias changes

--- connectors/list.go
+++ connectors/list.go
@@ -8,6 +8,7 @@ import (
 var List = make(map[string]reflect.Type)
 
 func init() {
+	List["alias"] = reflect.TypeOf(Alias{})
 	List["cli"] = reflect.TypeOf(Cli{})
 	List["bamboo"] = reflect.TypeOf(Bamboo{})
 	List["email"] = reflect.TypeOf(Email{})
--- models/message.go
+++ models/message.go
@@ -3,10 +3,11 @@ package models
 type Message struct {
 	Routes []Route
 	In     struct {
-		Source  string
-		User    string
-		Text    string
-		Process bool
+		Source    string
+		User      string
+		Text      string
+		Process   bool
+		Reprocess bool
 	}
 	Out struct {
 		Text   string
