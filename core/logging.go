5ddfc64459e4a54747008ad491d52d11e9703416
Matt McQuillan <automatedmatt@gmail.com>
2016-10-07 15:42:23 -0400

    adding console logging if no log file specified

--- core/logging.go
+++ core/logging.go
@@ -8,19 +8,23 @@ import (
 )
 
 func Logging(config *models.Config) {
-	if !FileExists(config.LogFile) {
-		nf, err := os.Create(config.LogFile)
-		if err != nil {
-			fmt.Println("Error - Cannot create log file at " + config.LogFile)
+	if config.LogFile == "" {
+		log.SetOutput(os.Stdout)
+	} else {
+		if !FileExists(config.LogFile) {
+			nf, err := os.Create(config.LogFile)
+			if err != nil {
+				fmt.Println("Error - Cannot create log file at " + config.LogFile)
+			}
+			nf.Close()
 		}
-		nf.Close()
+		f, err := os.OpenFile(config.LogFile, os.O_RDWR|os.O_APPEND, 0660)
+		if err != nil {
+			fmt.Println("Error - Cannot get to log file at " + config.LogFile)
+			panic(err)
+		}
+		log.SetOutput(f)
 	}
-	f, err := os.OpenFile(config.LogFile, os.O_RDWR|os.O_APPEND, 0660)
-	if err != nil {
-		fmt.Println("Error - Cannot get to log file at " + config.LogFile)
-		panic(err)
-	}
-	log.SetOutput(f)
 	log.Print("---")
 	log.Print("Starting jane bot...")
 }
