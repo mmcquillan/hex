4f4a3a09c8e1a1ac2d050f1a70a56584e794d53d
Ken Winner <kcswinner@gmail.com>
2016-12-18 17:28:53 -0500

    Adding some more logic to config data

--- data/configData.go
+++ data/configData.go
@@ -5,14 +5,39 @@ import ()
 // GetConfig Gets the config
 func GetConfig() (Config, error) {
 	var config Config
+	keyValues := make(map[string]string)
+
+	rows, err := Database.Query(`SELECT *
+                               FROM Config`)
+
+	if err != nil {
+		return config, err
+	}
+
+	for rows.Next() {
+		var key string
+		var value string
+		err = rows.Scan(&key, &value)
+
+		if err != nil {
+			return config, err
+		}
+
+		keyValues[key] = value
+	}
+
+	config.KeyValues = keyValues
 
 	return config, nil
 }
 
 // CreateConfig Create the config
 func CreateConfig(config Config) (Config, error) {
-
-	return config, nil
+	_, err := Database.Exec(`INSERT INTO Config
+                              ()
+                           VALUES
+                              ()`)
+	return config, err
 }
 
 // UpdateConfig Updates the config
