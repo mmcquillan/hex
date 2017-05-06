c168e7952090fef80e2699ccf4ac05648954a0d4
Matt McQuillan <automatedmatt@gmail.com>
2017-05-06 03:29:48 +0000

    Adding capability for list of servers in ssh action

--- actions/ssh.go
+++ actions/ssh.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	"github.com/hexbotio/hex/models"
@@ -16,64 +17,69 @@ type Ssh struct {
 //Action function
 func (x Ssh) Act(action models.Action, message *models.Message, config *models.Config) {
 
-	// have to lookup each matching server
-	for _, service := range config.Services {
-		if service.Active && service.Type == "ssh" {
-			if parse.Match(action.Service, service.Name) {
+	// break apart servers into list
+	servers := strings.Split(action.Service, ",")
+	for _, server := range servers {
 
-				command := parse.Substitute(action.Command, message.Inputs)
-				out := ""
+		// have to lookup each matching server
+		for _, service := range config.Services {
+			if service.Active && service.Type == "ssh" {
+				if parse.Match(server, service.Name) {
 
-				// setup the server connection
-				serverconn := true
-				clientconn := &ssh.ClientConfig{
-					User: service.Config["Login"],
-					Auth: []ssh.AuthMethod{
-						ssh.Password(service.Config["Pass"]),
-					},
-					HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-				}
-				port := "22"
-				if service.Config["Port"] != "" {
-					port = service.Config["Port"]
-				}
-				retryCounter := 1
-				for retryCounter <= 3 {
-					client, err := ssh.Dial("tcp", service.Config["Server"]+":"+port, clientconn)
-					if err != nil {
-						log.Print(err)
+					command := parse.Substitute(action.Command, message.Inputs)
+					out := ""
+
+					// setup the server connection
+					serverconn := true
+					clientconn := &ssh.ClientConfig{
+						User: service.Config["Login"],
+						Auth: []ssh.AuthMethod{
+							ssh.Password(service.Config["Pass"]),
+						},
+						HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 					}
-					if client == nil {
-						serverconn = false
-					} else {
-						defer client.Close()
-						session, err := client.NewSession()
+					port := "22"
+					if service.Config["Port"] != "" {
+						port = service.Config["Port"]
+					}
+					retryCounter := 1
+					for retryCounter <= 3 {
+						client, err := ssh.Dial("tcp", service.Config["Server"]+":"+port, clientconn)
 						if err != nil {
 							log.Print(err)
 						}
-						if session == nil {
+						if client == nil {
 							serverconn = false
 						} else {
-							defer session.Close()
-							b, err := session.CombinedOutput(command)
+							defer client.Close()
+							session, err := client.NewSession()
 							if err != nil {
 								log.Print(err)
 							}
-							out = string(b[:])
+							if session == nil {
+								serverconn = false
+							} else {
+								defer session.Close()
+								b, err := session.CombinedOutput(command)
+								if err != nil {
+									log.Print(err)
+								}
+								out = string(b[:])
+							}
+						}
+						if serverconn {
+							retryCounter = 999
+						} else {
+							time.Sleep(time.Duration(3*retryCounter) * time.Second)
+							retryCounter += 1
 						}
 					}
-					if serverconn {
-						retryCounter = 999
-					} else {
-						time.Sleep(time.Duration(3*retryCounter) * time.Second)
-						retryCounter += 1
+					if !serverconn {
+						out = "ERROR - Cannot connect to server " + service.Name
 					}
+					message.Response = append(message.Response, out)
+					message.Success = ActionSuccess(out, action.Success, action.Failure)
 				}
-				if !serverconn {
-					out = "ERROR - Cannot connect to server " + service.Name
-				}
-				message.Response = append(message.Response, out)
-				message.Success = ActionSuccess(out, action.Success, action.Failure)
 			}
 		}
 	}
