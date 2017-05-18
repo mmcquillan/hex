56b9c950f6478ef060e18bcb1238f27a906726c6
Matt McQuillan <automatedmatt@gmail.com>
2017-05-18 00:41:06 +0000

    updating systemd service file

--- core/params.go
+++ core/params.go
@@ -14,6 +14,7 @@ func Params(config *models.Config) {
 	configFile := flag.String("config", "", "Location of the config file")
 	validate := flag.Bool("validate", false, "Validate the config file")
 	showVersion := flag.Bool("version", false, "Version of HexBot")
+	startup := flag.Bool("startup", false, "Startup file for HexBot")
 	flag.Parse()
 
 	// set the config values
@@ -23,6 +24,25 @@ func Params(config *models.Config) {
 	// operate on any flags
 	if *showVersion {
 		fmt.Print("HexBot " + config.Version + "\n")
+		os.Exit(0)
+	}
+
+	// startup file
+	if *startup {
+		fmt.Print("\n")
+		fmt.Print("1. Create file: /etc/systemd/system/hex.service\n")
+		fmt.Print("--------------------------------\n")
+		fmt.Print("[Unit]\n")
+		fmt.Print("Description=HexBot\n")
+		fmt.Print("\n")
+		fmt.Print("[Service]\n")
+		fmt.Print("Type=simple\n")
+		fmt.Print("ExecStart=/usr/local/bin/hex\n")
+		fmt.Print("Restart=on-failure\n")
+		fmt.Print("--------------------------------\n")
+		fmt.Print("\n")
+		fmt.Print("2. Run: systemctl enable hex.service\n")
+		fmt.Print("\n")
 		os.Exit(0)
 	}
 
