d773778c09f820e837657b85a16988ec18e2a003
Matt McQuillan <automatedmatt@gmail.com>
2017-10-15 11:27:52 +0000

    formatting auditing output

--- outputs/auditing.go
+++ outputs/auditing.go
@@ -3,6 +3,7 @@ package outputs
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/hexbotio/hex/models"
@@ -12,12 +13,32 @@ type Auditing struct {
 }
 
 func (x Auditing) Write(message models.Message, config models.Config) {
+	who := " "
+	if message.Attributes["hex.user"] != "" {
+		who = who + message.Attributes["hex.user"] + " "
+	}
+	if message.Attributes["hex.channel"] != "" {
+		if strings.HasPrefix(message.Attributes["hex.channel"], "#") {
+			who = who + message.Attributes["hex.channel"] + " "
+		} else {
+			who = who + "DM "
+		}
+	}
+	if message.Attributes["hex.schedule"] != "" {
+		who = who + message.Attributes["hex.schedule"] + " "
+	}
+	if message.Attributes["hex.ipaddress"] != "" {
+		who = who + message.Attributes["hex.ipaddress"] + " "
+	}
+	if message.Attributes["hex.url"] != "" {
+		who = who + message.Attributes["hex.url"] + " "
+	}
+	out := fmt.Sprintf("%s [%s] %s\n", time.Now().Format(time.RFC3339), who, message.Attributes["hex.input"])
 	file, err := os.OpenFile(config.AuditingFile, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		config.Logger.Error("Auditing File", err)
 	}
 	defer file.Close()
-	out := fmt.Sprintf("%s %+v\n", time.Now().Format(time.RFC3339), message)
 	if _, err = file.WriteString(out); err != nil {
 		config.Logger.Error("Writing Audit File", err)
 	}
